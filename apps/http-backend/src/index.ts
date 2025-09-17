4956a45273ef5f3717a361e7773e1d53d645c5d7
Harshit Chauhan <chauhanharshit498@gmail.com>
2025-09-17 13:37:38 +0530

    commonpackages

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,14 +2,19 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { middleware } from "./middleware";
 import { JWT_SECRET } from "@repo/backend-common/config";
+import {createUserSchema} from "@repo/common/types";
+import { SigninSchema,CreateRoomSchema } from "@repo/common/types";
 const app = express();
-
-app.get('/',(req,res)=>{
-    res.json({message:"Harshit Chauhan"});
-})
 
 
 app.post('/signin',(req,res)=>{
+       const data =SigninSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"Incorrect Inputs"
+        })
+        return;
+    }
 const userId = 1;
 jwt.sign({
     userId
@@ -17,10 +22,23 @@ jwt.sign({
 })
 
 app.post('/signup',(req,res)=>{
-
+   const data =createUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"Incorrect Inputs"
+        })
+        return;
+    }
 })
 
 app.post('/room',middleware,(req,res)=>{
+   const data =CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"Incorrect Inputs"
+        })
+        return;
+    }
 
 })
 
